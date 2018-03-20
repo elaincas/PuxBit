db102b41a2062b8f5a00a251802996defe1a6eb0
LAINCAS0296\Lainfiesta <eliaslaincas@gmail.com>
2018-03-19 21:09:59 -0600

    Carga Inicial solo una entidad

---
+++ Dominio.Core/Entidades/EntidadBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Core.Entidades
+{
+    public class EntidadBase
+    {
+        public int UsuarioCrea { get; set; }
+        public int UsuarioModifica { get; set; }
+        public DateTime FechaCrea { get; set; }
+        public DateTime FechaModifica { get; set; }
+        public Boolean EsActivo { get; set; }
+
+        public EntidadBase()
+        {
+            FechaCrea = DateTime.Now;
+            EsActivo = true;
+        }
+    }
+}
---
+++ Dominio/Entidades/Alumnos.cs
@@ -0,0 +1,27 @@
+﻿using Dominio.Core.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entidades
+{
+    public class Alumnos : EntidadBase
+    {
+        public int IdAlumno { get; set; }
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public int Identidad { get; set; }
+        public string Pasaporte { get; set; }
+        public int Edad { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Sexo { get; set; }
+        public string TipoSangre { get; set; }
+        public string Telefono { get; set; }
+        public string Nacionalidad { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
